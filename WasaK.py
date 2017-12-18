62009868c580af23ff1585f7cfcc490a1961f817
Lucas <alivemindset@protonmail.com>
2017-12-18 04:56:37 -0200

    Client basic

--- WasaK.py
+++ WasaK.py
@@ -1,35 +1,71 @@
 #-*- coding: utf-8 -*-
-import socket
-import select
-import sys
-import os
-import time
+
+try:
+    import socket
+    import select
+    import sys
+    import os
+    import time
+    import random
+except Exception as err:
+    print("ERRO: "+err)
+    print("Não conseguimos importar as bibliotecas.")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 os.system('clear')
-print """\033[1m\033[31m
-             
-             <BANNER>
-
+print """\033[1m\033[32;1m
+                                <Cliente>
+         __       __                                __    __ 
+        |  \  _  |  \                              |  \  /  \\
+        | $$ / \ | $$  ______    _______   ______  | $$ /  $$
+        | $$/  $\| $$ |      \  /       \ |      \ | $$/  $$ 
+        | $$  $$$\ $$  \$$$$$$\|  $$$$$$$  \$$$$$$\| $$  $$  
+        | $$ $$\$$\$$ /      $$ \$$    \  /      $$| $$$$$\  
+        | $$$$  \$$$$|  $$$$$$$ _\$$$$$$\|  $$$$$$$| $$ \$$\ 
+        | $$$    \$$$ \$$    $$|       $$ \$$    $$| $$  \$$\\
+         \$$      \$$  \$$$$$$$ \$$$$$$$   \$$$$$$$ \$$   \$$
+         
+        \033[31mCriado por: \033[0;1mLucas Simoni <alivemindset@protonmail.com>
+        
 \033[0;0m
 """
-
-ipaddr = str(raw_input('IP: '))
-if ipaddr == '' or ipaddr == ' ':
-    ipaddr = '127.0.0.1'
-port = int(raw_input('PORTA: '))
-if port == '' or port == ' ':
-    port = 12786
 try:
-    server.connect((ipaddr, port))
-    print '[\033[32m+\033[0;0m] Conectado ao servidor...'
-    server.setblocking(False)
-except:
-    print '[\033[32m-\033[0;0m] Servidor não existe...'
+    ipaddr = str(raw_input('\033[1m[\033[32;1m*\033[0;1m] IP: '))
+    if ipaddr == '' or ipaddr == ' ':
+        ipaddr = '127.0.0.1'
+        print("\033[1m[\033[33;1m!\033[0;1m] IP padrão definido ==> " + ipaddr)
+    else:
+        print("\033[1m[\033[32;1m!\033[0;1m] IP definido ==> " + ipaddr)
+    port = raw_input('\033[1m[\033[32;1m*\033[0;1m] PORTA: ')
+    if str(port) == '' or str(port) == ' ':
+        port = 12786
+        print("\033[1m[\033[33;1m!\033[0;1m] PORTA padrão definida ==> " + str(port))
+    else:
+        print("\033[1m[\033[32;1m!\033[0;1m] PORTA definida ==> " + str(port))
+
+    nome = raw_input('\033[1m[\033[32;1m*\033[0;1m] Digite um nickname: ')
+    if nome == '' or nome == ' ':
+        nome = random.choice(('NoName','Anonimo','SemNome','Guest','Convidado', 'Anonymous'))+random.choice(('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'))
+        print("\033[1m[\033[33;1m!\033[0;1m] Nickname aleatorio definido ==> "+ nome)
+    else:
+        print("\033[1m[\033[33;1m!\033[0;1m] Nickname definido ==> " + nome)
+
+    try:
+        server.connect((ipaddr, int(port)))
+        print '[\033[32m+\033[0;1m] Conectado ao servidor...'
+        server.settimeout(5)
+        server.setblocking(False)
+        server.send('@+' + nome)
+        os.system('clear')
+    except:
+        print '[\033[31m-\033[0;1m] Servidor não existe...'
+        exit()
+except EOFError:
+    print '\n[\033[31m-\033[0;1m] Saindo do cliente...'
     exit()
-nome = raw_input('Digite um nickname: ')
-server.send('@+'+nome)
-os.system('clear')
+except KeyboardInterrupt:
+    print '\n[\033[31m-\033[0;1m] Saindo do cliente...'
+    exit()
 
 ant = False
 while True:
@@ -39,6 +75,16 @@ while True:
         for socks in read_sockets:
             if socks == server:
                 message = socks.recv(2048)
+                if str(message).rstrip().startswith('@-Servidor-OFF'):
+                    print('\n\n[\033[31m-\033[0;1m] O servidor foi fechado!')
+                    print '[\033[31m-\033[0;1m] Volte logo ' + nome
+                    server.close()
+                    exit()
+                elif str(message).rstrip().startswith('@-Servidor-FULL'):
+                    print('\n\n[\033[31m-\033[0;1m] O servidor está lotado!')
+                    print '[\033[31m-\033[0;1m] Volte logo ' + nome
+                    server.close()
+                    exit()
                 print '\n '+message
                 ant = True
                 print " \033[1m\033[31m"+nome+"\033[0;0m» ",
@@ -61,14 +107,20 @@ while True:
                     time.sleep(0.2)
                     continue
 
+                if message.lower().rstrip() == '':
+                    continue
+
                 server.send(nome+"» "+message)
                 sys.stdout.flush()
     except KeyboardInterrupt:
-        print 'Volte logo ' + nome
+        server.send('@-'+nome)
+        print '\n\n[\033[31m-\033[0;1m] Volte logo ' + nome
         server.close()
         exit()
     except EOFError:
-        print 'Volte logo ' + nome
+        server.send('@-'+nome)
+        print '\n\n[\033[31m-\033[0;1m] Volte logo ' + nome
         server.close()
         exit()
+
 server.close()
